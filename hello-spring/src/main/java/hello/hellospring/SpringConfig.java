6e63ac38cbbb49d03030d3e788970ad5fe18b804
yeongsang2 <dudtkd1221@gmail.com>
2022-06-18 17:12:59 +0900

    스프링데이터JPA AOP

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,7 +1,9 @@
 package hello.hellospring;
+import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,23 +14,30 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig { //spring config를 이용한 DI(dependency injection)
 
-    private EntityManager em;
 
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
+
+    public TimeTraceAop timeTraceAop(){
+        return new TimeTraceAop();
+    }
+
+
+    //@Bean
+   // public MemberRepository memberRepository() {
 // return new MemoryMemberRepository();
      //   return new JdbcMemberRepository(dataSource);
        // return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+        //return new JpaMemberRepository(em);
+    //}
 }
