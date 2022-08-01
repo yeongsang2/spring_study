b561ee682a794d2261b99ad7b37c18c3bdbd7d8f
yeongsang2 <dudtkd1221@gmail.com>
2022-08-01 16:38:35 +0900

    상품 엔티티 개발(비즈니스 로직 추가)

--- jpashop/src/main/java/com/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/com/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package com.jpabook.jpashop.domain.item;
 
 import com.jpabook.jpashop.domain.Category;
+import com.jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -27,6 +28,25 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    //==비즈니스 로직==// data를 가지고 있는 entity에서 비즈니스 로직 처리 -> 응집도 높음
 
+    /**
+     * stock 재고 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stcok 재고 감소
+     */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if( restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity -= quantity;
+
+    }
 
 }
