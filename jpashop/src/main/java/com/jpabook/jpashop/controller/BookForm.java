7c53fa4f66416a79f303901f900e436943d4195b
yeongsang2 <dudtkd1221@gmail.com>
2022-08-02 16:54:03 +0900

    상품 수정

--- jpashop/src/main/java/com/jpabook/jpashop/controller/BookForm.java
+++ jpashop/src/main/java/com/jpabook/jpashop/controller/BookForm.java
@@ -9,11 +9,9 @@ import lombok.Setter;
 public class BookForm {
 
     private Long id;
-
     private String name;
     private int price;
     private int stockQuantity;
-
     private String author;
     private String isbn;
 
